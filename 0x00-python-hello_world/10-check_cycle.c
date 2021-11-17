728ee2154642d7d7f54b88b973fa32a729ce113b
KobinaOtc <kobinaotchereadjoku@gmail.com>
2021-11-17 20:13:37 +0000

    optim

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * check_cycle - checks if singly linked list is a cycle
+ * Return: 0 if no cycle, 1 is yes
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *fast = list;
+	listint_t *slow = list;
+
+	if (!list)
+		return (0);
+
+	while (1)
+	{
+		if (fast->next != NULL && fast->next->next != NULL)
+		{
+			fast = fast->next->next;
+			slow = slow->next;
+
+			if (fast == slow) /*if nodes match, cycle found*/
+				return (1);
+		}
+		else
+			return (0);
+	}
+
+}
