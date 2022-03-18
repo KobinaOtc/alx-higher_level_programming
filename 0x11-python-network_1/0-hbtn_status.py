6adf75538af5d020ac1d228ccedee1de85f5e1aa
kobinaotc <kobinaotchereadjoku@gmail.com>
2022-03-18 11:53:48 +0000

    bug fixam

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-"""
-Prints different results of
-a request
-"""
-import urllib.request
+'''Intranet script'''
 
 
-if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
+        content = res.read()
         print("Body response:")
-        print("-\t type:", type(html))
-        print("-\t content:", html)
-        print("-\t utf8 content:", html.decode("utf-8"))
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
